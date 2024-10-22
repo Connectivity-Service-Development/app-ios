500b3a9a3bb99b97cb73ecfe6e487f89e7f325a0
Daniel Gabzdyl <daniel.gabzdyl@eman.cz>
2024-10-22 09:32:09 +0200

    chore: project setup

---
+++ Skoda Practical Evaluation/Skoda_Practical_EvaluationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Skoda_Practical_EvaluationApp.swift
+//  Skoda Practical Evaluation
+//
+//  Created by Daniel Gabzdyl on 22.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct Skoda_Practical_EvaluationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Skoda Practical EvaluationTests/Skoda_Practical_EvaluationTests.swift
@@ -0,0 +1,17 @@
+//
+//  Skoda_Practical_EvaluationTests.swift
+//  Skoda Practical EvaluationTests
+//
+//  Created by Daniel Gabzdyl on 22.10.2024.
+//
+
+import Testing
+@testable import Skoda_Practical_Evaluation
+
+struct Skoda_Practical_EvaluationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
